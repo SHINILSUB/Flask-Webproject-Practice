a7c3459994160fde2d8ab940b86214acc2f6edae
miniPinetree <68773118+miniPinetree@users.noreply.github.com>
2021-03-02 19:33:23 +0900

    가상환경 업데이트

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\misong\Desktop\hh99\jinjjasan\venv
+set VIRTUAL_ENV=C:\Users\misong\Desktop\hh99\test\hh99_Webproject_w1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
