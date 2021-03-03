6ff64ef382394f536c8baa435b86fb52ca5cb5f4
miniPinetree <68773118+miniPinetree@users.noreply.github.com>
2021-03-03 22:39:21 +0900

    JWT인증방식 로그인 구현, 토큰 유효성 검증 함수 분리

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import jwt
-import datetime
-import hashlib
+import datetime # 토큰 만료시간을 주기위한 모듈
+import hashlib # 비밀번호 암호화 모듈
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
@@ -10,6 +10,7 @@ from datetime import datetime, timedelta
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# jwt 토큰을 만들 때 필요한 비밀문자열
 SECRET_KEY = 'SPARTA'
 
 client = MongoClient('localhost', 27017)
@@ -18,25 +19,34 @@ db = client.dbsparta_plus_week4
 
 @app.route('/')
 def home():
+        return render_template('01_main.html')
+
+## jwt 토큰 유효성 검사
+def login_decorator(func):
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
-        return render_template('01_main.html')
+        user_info = db.user.find_one({"id": payload['id']})
+        return render_template('index.html', nickname=user_info["nick"])
     except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+        return redirect(url_for("02_login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+        return redirect(url_for("02_login", msg="로그인 정보가 존재하지 않습니다."))
 
-
+## 로그인페이지 렌더링
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
-    return render_template('login.html', msg=msg)
+    return render_template('02_login.html', msg=msg)
 
+## 지도페이지 렌더링
+@app.route('/map')
+def test_map():
+    return render_template('03_map.html')
+
+# 입력값이 db에 존재한다면 클라이언트에 토큰을 전달한다.
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
-    # 로그인
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
 
@@ -44,28 +54,29 @@ def sign_in():
     result = db.users.find_one({'username': username_receive, 'password': pw_hash})
 
     if result is not None:
+        # payload란 토큰을 디코딩하면 나오게 될 사용자에 대한 정보이다.
         payload = {
         'id': username_receive,
-         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 3)  # 로그인 3시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
-        return jsonify({'result': 'success', 'token': token})
-    # 찾지 못하면
+        return jsonify({'result': 'success', 'token': token })
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
-
+# 회원가입 API
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
-    # 회원가입
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
+    nickname_receive = request.form['nickname_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    # DB에 저장
+    # DB에 username, nickname, 암호화한 password 저장
     doc = {
-        "username": username_receive,                               # 아이디
-        "password": password_hash,                                  # 비밀번호                                        # 프로필 한 마디
+        "username": username_receive,
+        "password": password_hash,
+        "nickname": nickname_receive                                                                         # 프로필 한 마디
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
